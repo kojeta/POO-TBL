fffc9245d46ee47c3ffc3d69f048d79d8ceed808
Thiago Martins <72246747+kojeta@users.noreply.github.com>
2026-08-26 20:35:58 -0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,15 @@
+﻿Persona persona1 = new Persona();
+persona1.Name = "joazinho";
+
+Persona persona2 = persona1;
+
+persona2.Name = "pedrinho";
+
+Console.WriteLine(persona1.Name);
+Console.WriteLine(persona2.Name);
+
+class Persona
+{
+    public string Name;
+}
+
