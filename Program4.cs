8113681aa6b4e6e8c14da5cb7d76235aa8d87286
Thiago Martins <72246747+kojeta@users.noreply.github.com>
2026-08-26 21:15:22 -0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Por favor, digite um número inteiro:");
+string entrada = Console.ReadLine();
+
+if (int.TryParse(entrada, out int numero))
+{
+    Console.WriteLine($"Você digitou: {numero}");
+}
+else
+{
+    Console.WriteLine("Erro: O valor digitado não é um número inteiro válido.");
+}
